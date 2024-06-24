6490ed4381b5d63a6c904177ee33dda85c7e8234
CANguru-System <gustavwostrack@web.de>
2024-06-24 18:53:20 +0200

    Update CANguru.h

--- 0101-CANguru-Bridge-Olimex-Version-3.5/lib/src/CANguru.h
+++ 0101-CANguru-Bridge-Olimex-Version-3.5/lib/src/CANguru.h
@@ -12,7 +12,7 @@
 // Bmp-Logo of an CANguru
 #include <stdint.h>
 
-const String CgVersionnmbr = "3.50";
+const String CgVersionnmbr = "3.50a";
 const String CgVersion = "CANguru " + CgVersionnmbr;
 
 #endif // CANGURU_H
