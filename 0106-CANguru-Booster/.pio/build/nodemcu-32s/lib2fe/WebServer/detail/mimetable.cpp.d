9d12eb13284b954be53077da40ee72caefd07862
CANguru-System <gustavwostrack@web.de>
2024-07-11 19:46:15 +0200

    Update

--- 0106-CANguru-Booster/.pio/build/nodemcu-32s/lib2fe/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/nodemcu-32s/lib2fe/WebServer/detail/mimetable.cpp.o: \
- C:/Users/gusta/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
- C:/Users/gusta/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
- C:/Users/gusta/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
