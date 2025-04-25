c380354571c9bb78a46696776024a4fb5f84dee1
CANguru-System <gustavwostrack@web.de>
2025-04-25 14:04:53 +0200

    Update light

--- 0109-Hausbeleuchtung-ESP32C3/src/main.cpp
+++ 0109-Hausbeleuchtung-ESP32C3/src/main.cpp
@@ -10,8 +10,10 @@ Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_RGB + NEO_KHZ800);
 Preferences preferences;
 #define prefName "HOUSELIGHT"
 #define setup_done 0x47
+#define setup_NOTdone 0xFF
 
-uint8_t cnt;
+const char del = '!';
+
 
 unsigned long lastTime; // timestamp in us of when the last step was taken
 unsigned long baseTime; // delay between steps, in us, based on speed
@@ -34,6 +36,9 @@ struct roomType
   uint16_t duration;
 };
 roomType rooms[LED_COUNT];
+
+String command;
+String load;
 
 int16_t random(int MaxZahl)
 {
@@ -59,12 +64,15 @@ void setup()
     for (uint8_t r = 0; r < strip.numPixels(); r++)
     {
       // changeable values
-      rooms[r].onTime = random(10) + r;
+      // onTime
+      rooms[r].onTime = r+1; //random(10) + r;
       sprintf(buffer, "onTime%d", r);
       preferences.putUShort(buffer, rooms[r].onTime);
+      // offTime
       rooms[r].offTime = random(5) + 2 * r;
       sprintf(buffer, "offTime%d", r);
       preferences.putUShort(buffer, rooms[r].offTime);
+      // brightness
       rooms[r].brightness = 16;
       sprintf(buffer, "brightness%d", r);
       preferences.putUShort(buffer, rooms[r].brightness);
@@ -105,147 +113,138 @@ void setup()
   strip.show();
 
   lastTime = millis();
-  cnt = 4;
+  Serial.println("BACK");
 }
 
 void loop()
 {
-  String command;
-  String subCommand1;
-  String subCommand3;
-  String load;
-  
-  if (Serial.available() == cnt)
+
+  if (Serial.available() > 0)
   {
-    command = Serial.readString(); //('\n'); // read string until newline character
-    subCommand1 = command.substring(0, 1);
-    subCommand3 = command.substring(0, 3);
-    //
-    // Mit dem Kommando DECX werden die Daten des Raumes X (zwischen 0 und 7) zu einem String ontime!offtime!brightness!basetime
-    // zusammengefügt und übermittelt
-    if (subCommand3 == "DEC")
+    //  command = Serial.readStringUntil('$'); // read string until newline character  {
+
+    // read the incoming byte:
+    char inByte = Serial.read();
+    command += inByte;
+    if (inByte == '$')
     {
-      uint8_t d = command.charAt(3) - 0x30;
-      load = String(rooms[d].onTime) + "!" + String(rooms[d].offTime) + "!" + String(rooms[d].brightness) + "!" + baseTime;
-      Serial.println("&1A");
-      Serial.println(load);
-      subCommand3.clear();
-      command.clear();
-      exit;
-    }
-    // Alle gefundenen USB-Ports werden mit "USB0" aufgerufen.
-    // Nur die richtigen Hausbeleuchtungen antworten mit "USB"
-    if (command == "USB0")
-    {
-      Serial.println("&1B");
-      Serial.println("USB");
-      command.clear();
-      exit;
-    }
-    // Mit TXXX wird die Länge (XXX) des folgenden Kommandos übermittelt
-    if (subCommand1 == "T")
-    {
-      String sCnt = command.substring(1, 4);
-      cnt = sCnt.toInt();
-      Serial.println("&1C");
-      Serial.println(cnt);
-      subCommand1.clear();
-      command.clear();
-      exit;
-    }
-    // Ein neuer geänderter Datensatz wird mit der Länge XXX übermittelt. Der Trenner ist ein "!"
-    // L!index!ontime!offtime!brightness!basetime
-    // L!3!27!39!32!1500
-    // cnt = 12;
-    if (subCommand1 == "L")
-    {
-      int startIndex = 0;
-      char del = '!';
-      int8_t index;
-      String indx;
-      uint8_t substr = 0;
-      char buffer[25]; // Enough to hold 3 digits and a null terminator
-      for (int i = 0; i < command.length(); i++)
+      //
+      // Mit dem Kommando DECX werden die Daten des Raumes X (zwischen 0 und 7) zu einem String ontime!offtime!brightness!basetime
+      // zusammengefügt und übermittelt
+      if (command.substring(0, 3) == "DEC")
       {
-        if (command[i] == del)
-        {
-          String substring = command.substring(startIndex, i);
-          switch (substr)
-          {
-          case 0:
-            // "L"
-            substr++;
-            break;
-          case 1:
-            // index
-            index = substring.toInt();
-            substr++;
-            break;
-          // changeable values
-          case 2:
-            // lightOn
-            sprintf(buffer, "onTime%d", index);
-            rooms[index].onTime = substring.toInt();
-            preferences.putUShort(buffer, rooms[index].onTime);
-            substr++;
-            break;
-          case 3:
-            // lightOn
-            sprintf(buffer, "offTime%d", index);
-            rooms[index].offTime = substring.toInt();
-            preferences.putUShort(buffer, rooms[index].offTime);
-            substr++;
-            break;
-          case 4:
-            // lightOn
-            sprintf(buffer, "brightness%d", index);
-            rooms[index].brightness = substring.toInt();
-            preferences.putUShort(buffer, rooms[index].brightness);
-            substr++;
-            break;
-          default:
-            break;
-          }
-          startIndex = i + 1;
-        }
+        Serial.println("&A");
+        delay(50);
+        uint8_t d = command.charAt(3) - 0x30;
+        load = String(rooms[d].onTime) + del + String(rooms[d].offTime) + del + String(rooms[d].brightness) + del + baseTime;
+        Serial.println(load);
       }
-      baseTime = command.substring(startIndex).toInt();
-      preferences.putUShort("baseTime", baseTime);
-      // unchangeable values
-      rooms[index].colorOn = strip.Color(rooms[index].brightness, rooms[index].brightness, rooms[index].brightness);
-      rooms[index].colorOff = strip.Color(0, 0, 0);
-      rooms[index].status = lightoff;
-      rooms[index].goneTime = 0;
-      rooms[index].duration = rooms[index].onTime + rooms[index].offTime;
-      strip.setPixelColor(index, strip.Color(0, 0, 0));
-      Serial.println("&1D");
-      cnt = 4;
-      subCommand1.clear();
-      command.clear();
-      exit;
+      if (command.substring(0, 3) == "RES")
+      {
+        // setup_done auf "TRUE" setzen
+        preferences.putUChar("setup_done", setup_NOTdone);
+        ESP.restart();
+      }
+      // Alle gefundenen USB-Ports werden mit "USB0" aufgerufen.
+      // Nur die richtigen Hausbeleuchtungen antworten mit "USB"
+      if (command.substring(0, 4) == "USB0")
+      {
+        Serial.println("&B");
+        delay(50);
+        Serial.println("USB");
+      }
+      // Ein neuer geänderter Datensatz wird mit der Länge XXX übermittelt. Der Trenner ist ein "!"
+      // L!index!ontime!offtime!brightness!basetime
+      // L!3!27!39!32!1500$
+      // cnt = 12;
+      if (command.substring(0, 1) == "L")
+      {
+        int startIndex = 0;
+        int8_t index;
+        String indx;
+        uint8_t substr = 0;
+        char buffer[25]; // Enough to hold 3 digits and a null terminator
+        Serial.println("&C");
+        delay(50);
+        for (int i = 0; i < command.length(); i++)
+        {
+          if (command[i] == del)
+          {
+            String substring = command.substring(startIndex, i);
+            switch (substr)
+            {
+            case 0:
+              // "L"
+              substr++;
+              break;
+            case 1:
+              // index
+              index = substring.toInt();
+              substr++;
+              break;
+            // changeable values
+            case 2:
+              // lightOn
+              sprintf(buffer, "onTime%d", index);
+              rooms[index].onTime = substring.toInt();
+              preferences.putUShort(buffer, rooms[index].onTime);
+              substr++;
+              break;
+            case 3:
+              // lightOff
+              sprintf(buffer, "offTime%d", index);
+              rooms[index].offTime = substring.toInt();
+              preferences.putUShort(buffer, rooms[index].offTime);
+              substr++;
+              break;
+            case 4:
+              // brightness
+              sprintf(buffer, "brightness%d", index);
+              rooms[index].brightness = substring.toInt();
+              preferences.putUShort(buffer, rooms[index].brightness);
+              substr++;
+              break;
+            default:
+              break;
+            }
+            startIndex = i + 1;
+          }
+        }
+        // baseTime
+        baseTime = command.substring(startIndex).toInt();
+        preferences.putUShort("baseTime", baseTime);
+        // unchangeable values
+        rooms[index].colorOn = strip.Color(rooms[index].brightness, rooms[index].brightness, rooms[index].brightness);
+        rooms[index].colorOff = strip.Color(0, 0, 0);
+        rooms[index].status = lightoff;
+        rooms[index].goneTime = 0;
+        rooms[index].duration = rooms[index].onTime + rooms[index].offTime;
+        strip.setPixelColor(index, strip.Color(0, 0, 0));
+      }
+      command = "";
     }
   }
-/*  if (millis() - lastTime >= baseTime)
-  {
-    // get the timeStamp of when you stepped:
-    lastTime = millis();
-    for (uint8_t r = 0; r < strip.numPixels(); r++)
+    if (millis() - lastTime >= baseTime)
     {
-      rooms[r].goneTime++;
-      if (rooms[r].goneTime > rooms[r].duration)
+      // get the timeStamp of when you stepped:
+      lastTime = millis();
+      for (uint8_t r = 0; r < strip.numPixels(); r++)
       {
-        rooms[r].goneTime = 0;
-        rooms[r].status = lightoff;
+        rooms[r].goneTime++;
+        if (rooms[r].goneTime > rooms[r].duration)
+        {
+          rooms[r].goneTime = 0;
+          rooms[r].status = lightoff;
+        }
+        if (rooms[r].goneTime > rooms[r].offTime)
+        {
+          rooms[r].status = lightOn;
+        }
+        if (rooms[r].status == lightOn)
+          strip.setPixelColor(r, rooms[r].colorOn);
+        else
+          strip.setPixelColor(r, rooms[r].colorOff);
+        strip.show();
       }
-      if (rooms[r].goneTime > rooms[r].offTime)
-      {
-        rooms[r].status = lightOn;
-      }
-      if (rooms[r].status == lightOn)
-        strip.setPixelColor(r, rooms[r].colorOn);
-      else
-        strip.setPixelColor(r, rooms[r].colorOff);
-      strip.show();
     }
-  }*/
 }
