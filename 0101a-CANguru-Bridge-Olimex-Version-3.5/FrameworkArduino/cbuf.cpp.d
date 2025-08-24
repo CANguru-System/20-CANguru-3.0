5cccb042ed5afef21f5e30a2fb1b6598c51736a4
CANguru-System <gustavwostrack@web.de>
2025-08-24 10:44:02 +0200

    Deleted files

--- 0101a-CANguru-Bridge-Olimex-Version-3.5/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32-evb/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/gusta/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/gusta/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
--- 0101a-CANguru-Bridge-Olimex-Version-3.5/lib10f/0100-CANguru-Library/OTA_include.cpp.d
+++
@@ -1,2 +0,0 @@
-.pio/build/esp32-evb/lib10f/0100-CANguru-Library/OTA_include.cpp.o: \
- .pio/libdeps/esp32-evb/0100-CANguru-Library/OTA_include.cpp
