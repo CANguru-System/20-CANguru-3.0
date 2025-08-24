5711583db03649258386df3bef2d85ff4d3dcdda
CANguru-System <gustavwostrack@web.de>
2025-08-24 10:36:48 +0200

    added firmware

---
+++ 0101a-CANguru-Bridge-Olimex-Version-3.5/FrameworkArduino/USBCDC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32-evb/FrameworkArduino/USBCDC.cpp.o: \
+ C:/Users/gusta/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBCDC.cpp \
+ C:/Users/gusta/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
+ C:/Users/gusta/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
---
+++ 0101a-CANguru-Bridge-Olimex-Version-3.5/lib2fe/WebServer/detail/mimetable.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32-evb/lib2fe/WebServer/detail/mimetable.cpp.o: \
+ C:/Users/gusta/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
+ C:/Users/gusta/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
+ C:/Users/gusta/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
